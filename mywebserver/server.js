3cb06a7eb5326db39ccf7f8a2ccb0f5cac9c8917
DoQuocBinh <binhdq@fpt.edu.vn>
2022-11-18 11:59:11 +0700

    session 2

---
+++ mywebserver/server.js
@@ -0,0 +1,31 @@
+//su dung thu vien http cua NodeJS
+var http= require('http')
+
+//tao mot server
+var server = http.createServer(function(req,res){
+    // set response header
+    if(req.url =='/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write("<h1>Home page</h1>")
+        res.write("<a href='/student'>Student page</a>")
+    }else if(req.url=='/student'){
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write("Student")
+    }else if (req.url == '/data') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ message: "Hello World"}));  
+    }
+    else
+    {
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        res.write("page not found!")
+    }
+    res.end()
+})
+
+//start server o mot port
+const PORT = 5000
+server.listen(PORT)
+console.log("Server is runing!")
+
+
